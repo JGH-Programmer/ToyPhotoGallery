a22f748aa679a21a015410299c2c40e397883165
JEONG GIHOON <rlgns927@gmail.com>
2023-02-09 01:47:55 +0900

    Initial commit

--- app/src/main/java/git/myapplication/toyphotogallery/AccountPage/Login.kt
+++ app/src/main/java/git/myapplication/toyphotogallery/AccountPage/Login.kt
@@ -2,6 +2,9 @@ package git.myapplication.toyphotogallery.AccountPage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
+import git.myapplication.toyphotogallery.DBHelper
 import git.myapplication.toyphotogallery.databinding.ActivityLoginBinding
 
 class Login : AppCompatActivity() {
@@ -13,5 +16,24 @@ class Login : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        binding.LoginActivityLoginButton.setOnClickListener {
+            val usertx = binding.LoginActivityAccountEditText.text.toString()
+            val passwordtx = binding.LoginActivityPasswordEditText.text.toString()
+
+            if (TextUtils.isEmpty(usertx) || TextUtils.isEmpty(passwordtx)) {
+                Toast.makeText(this, "Add Acount & Password ", Toast.LENGTH_SHORT).show()
+            } else {
+                val checkuser = DBHelper(this).checkuserpass(usertx, passwordtx)
+                if (checkuser == true) {
+                    Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show()
+                    //intent Activity
+                } else {
+                    Toast.makeText(this, "Wrong Username & Password", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+
+        }
     }
 }
