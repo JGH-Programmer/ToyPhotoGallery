0d12720eac4a6681d59682a67b0bf156f9a35201
JEONG GIHOON <rlgns927@gmail.com>
2023-02-09 23:56:47 +0900

    Initial commit

--- app/src/main/java/git/myapplication/toyphotogallery/AccountPage/SignUp.kt
+++ app/src/main/java/git/myapplication/toyphotogallery/AccountPage/SignUp.kt
@@ -1,7 +1,12 @@
 package git.myapplication.toyphotogallery.AccountPage
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
+import git.myapplication.toyphotogallery.DBHelper
+import git.myapplication.toyphotogallery.MainActivity
 import git.myapplication.toyphotogallery.databinding.ActivitySignUpBinding
 
 class SignUp : AppCompatActivity() {
@@ -14,6 +19,39 @@ class SignUp : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        binding.SignActivityLoginButton.setOnClickListener {
+            val unametext = binding.SignActivityAccountEditText.text.toString()
+            val pwordtext = binding.SignActivityPasswordEditText.text.toString()
+            val cpwordtext = binding.SignActivityConformPasswordEditText.text.toString()
+            val saveData = DBHelper(this).insertdata(unametext, pwordtext)
+
+            if (TextUtils.isEmpty(unametext) || TextUtils.isEmpty(pwordtext) || TextUtils.isEmpty(
+                    cpwordtext
+                )
+            ) {
+                Toast.makeText(
+                    this,
+                    "Add Username, Password & Conform Password",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                if (pwordtext.equals(cpwordtext)) {
+                    if (saveData == true) {
+                        Toast.makeText(this, "SignUp Successful", Toast.LENGTH_SHORT).show()
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this, "User Exists", Toast.LENGTH_SHORT).show()
+                    }
+                } else {
+                    Toast.makeText(this, "Password Not Match", Toast.LENGTH_SHORT).show()
+                }
+
+
+            }
+        }
 
     }
+
+
 }
